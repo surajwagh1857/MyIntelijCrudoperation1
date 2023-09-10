15e2937e8ddaa2900afacc34487aac8ee917163f
surajwagh1857 <surajwagh1995@gmail.com>
2023-09-10 15:29:42 +0530

    Mi third commit

--- src/main/java/com/demo/myspringbootapplicaton/MySpringBootApplicatonApplication.java
+++ src/main/java/com/demo/myspringbootapplicaton/MySpringBootApplicatonApplication.java
@@ -9,6 +9,7 @@ public class MySpringBootApplicatonApplication {
     public static void main(String[] args) {
         SpringApplication.run(MySpringBootApplicatonApplication.class, args);
         System.out.println("hi this is my My springboot application....");
+        System.out.println("this is  Myspringboot application....");
     }
 
 }
